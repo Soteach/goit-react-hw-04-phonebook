097c13420a4488bbaffbd4616a340947189f86b8
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-02-28 18:54:46 +0200

    rewrite filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,22 @@
-import { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Filter.css';
 
-export class Filter extends Component {
-  handlerChange = e => {
-    const filteredValue = e.currentTarget.value;
-    this.props.changeFilter(filteredValue);
-  };
+export const Filter = ({ changeFilter }) => {
+  return (
+    <label className="FilterLabel" htmlFor="">
+      Find contacts by name
+      <input
+        className="FilterInput"
+        type="text"
+        name="name"
+        onChange={e => {
+          changeFilter(e.currentTarget.value);
+        }}
+      />
+    </label>
+  );
+};
 
-  render() {
-    return (
-      <label className="FilterLabel" htmlFor="">
-        Find contacts by name
-        <input
-          className="FilterInput"
-          type="text"
-          name="name"
-          onChange={this.handlerChange}
-        />
-      </label>
-    );
-  }
-}
+Filter.propTypes = {
+  changeFilter: PropTypes.func.isRequired,
+};
